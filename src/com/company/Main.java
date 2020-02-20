2dd2015a332d38744d6540b4ca05d9ef96cb0232
Rebecca <Rebecca.powers.2021@student.lhschools.org>
2020-02-20 14:01:29 -0500

    intellij Commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main{
+
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+        System.out.print("My name is ");
+        System.out.println("Rebecca.");
+        System.out.println("This summer I went to Florida to visit my family. ");
+        System.out.println("I also went to Cincinnati for my birthday. ");
+        System.out.println("I turned seventeen.");
+
+    }
+}
